9a5858064ecea77ec29f25416875f4fa8bd762d0
rusthigh <rusthigh@proton.me>
2022-06-08 12:34:20 +0000

    Remove unused routes

--- iNLTK/SetLanguage.py
+++ iNLTK/SetLanguage.py
@@ -22,4 +22,32 @@ setup('ml')
 setup('ne')
 
 #Oriya
-setup('or
+setup('or')
+
+#Marathi
+setup('mr')
+
+#Bengali
+setup('bn')
+
+#Tamil
+setup('ta')
+
+#Urdu
+setup('ur')
+
+#Telugu
+setup('te')
+
+#English
+setup('en')
+
+#Code-mixed languages
+#Hinglish (Hindi and English)
+setup('hi-en')
+
+#Tanglish (Tamil and English)
+setup('ta-en')
+
+#Manglish (Malayalam and English)
+setup('ml-en')
---
+++ indic-nlp-library/Indicization.py
@@ -0,0 +1,6 @@
+from indicnlp.transliterate.unicode_transliterate import ItransTransliterator
+
+input_text='pAlakkAda'
+# input_text='pitL^In'
+lang='ml'
+x=It
