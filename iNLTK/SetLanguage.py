6d82db597551ace15aeb886de4e16e061055322a
rusthigh <rusthigh@proton.me>
2022-06-08 08:09:12 +0000

    gitignore updates

---
+++ iNLTK/SetLanguage.py
@@ -0,0 +1,25 @@
+from inltk.inltk import setup
+#Native Languages
+#Hindi
+setup('hi')
+
+#Panjabi
+setup('pa')
+
+#Sanskrit
+setup('sa')
+
+#Gujarati
+setup('gu')
+
+#Kannada
+setup('kn')
+
+#Malayalam
+setup('ml')
+
+#Nepali
+setup('ne')
+
+#Oriya
+setup('or
